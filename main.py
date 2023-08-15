bd773243bb1f73485b9cd35067aa561529bd83c0
Ali Asghar Marvi <aliasghar_marvi52@hotmail.com>
2023-08-15 11:31:24 +0200

    updated model evaluation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -33,6 +35,29 @@ try:
     logger.info(f"----------- stage {STAGE_NAME} started -------------")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f"----------- stage {STAGE_NAME} finished -------------")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Training Stage"
+try:
+    logger.info(f"----------- stage {STAGE_NAME} started -------------")
+    model_training = ModelTrainerTrainingPipeline()
+    model_training.main()
+    logger.info(f"----------- stage {STAGE_NAME} finished -------------")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f"----------- stage {STAGE_NAME} started -------------")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f"----------- stage {STAGE_NAME} finished -------------")
 except Exception as e:
     logger.exception(e)
